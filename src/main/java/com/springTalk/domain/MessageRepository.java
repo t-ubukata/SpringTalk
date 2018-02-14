e7f4950eecce9ca612599f91066e94a3fbc39aba
Tomohiro Ubukata <ykmr729@gmail.com>
2018-02-14 20:20:16 +0900

    fix query

--- src/main/java/com/springTalk/domain/MessageRepository.java
+++ src/main/java/com/springTalk/domain/MessageRepository.java
@@ -15,7 +15,8 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
   List<Message> findBySenderIdOrderById(Long senderId);
 
   // native query dependent on Oracle DB
-  @Query(value = "select * " + "from app.messages " + "where id "
-      + "in (select min(id) as id from app.messages group by room_id) " + "order by id desc;", nativeQuery = true)
+  final String QUERY = "select * from app.messages where id in (select min(id) as id from app.messages group by room_id) order by id desc;";
+
+  @Query(value = QUERY, nativeQuery = true)
   List<Message> findFirstMessageOfRoomOrderByIdDesc();
 }
