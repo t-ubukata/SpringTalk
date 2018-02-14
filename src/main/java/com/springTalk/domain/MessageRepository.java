e2c284021924fc65fecd3d61ff852cb967c5ba3e
Tomohiro Ubukata <ykmr729@gmail.com>
2018-02-14 22:10:34 +0900

    change URI getting method

--- src/main/java/com/springTalk/domain/MessageRepository.java
+++ src/main/java/com/springTalk/domain/MessageRepository.java
@@ -16,6 +16,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
   List<Message> findBySenderIdOrderById(Long senderId);
 
   // native query dependent on Oracle DB
+  // note: not to add semicolon to query
   final String QUERY = "select * from app.messages where id in (select min(id) as id from app.messages group by room_id) order by id desc";
 
   @Query(value = QUERY, nativeQuery = true)
